b0da818c8d9af37c0154ebb620e16c2cb79bd6dd
YS739 <suk921@gmail.com>
2022-12-13 15:57:08 +0900

    page connect check

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ToDoList from "../pages/TodoList";
+// import ToDoList from "../pages/ToDoList";
+import Home from "../pages/Home";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ToDoList />} />
+        {<Route path="/" element={<Home />} />}
+        {/* <Route path="/" element={<ToDoList />} /> */}
         {/* <Route path="/:id" element={<Work />} /> */}
         {/* work 대신 Todo 넣기(import하고) */}
       </Routes>
