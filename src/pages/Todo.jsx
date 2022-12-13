17e479a37464cd69713831d39b89a8abc78463f1
YS739 <suk921@gmail.com>
2022-12-13 16:08:03 +0900

    edit todo.jsx can't use useState in Todo.jsx - error

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -2,10 +2,10 @@
 
 import { useParams } from "react-router-dom";
 
-const [toDoS, setToDoS] = useState([
+const toDoS = [
   { id: 0, title: "리액트", content: "리액트 과제 다시 하기", isDone: false },
   { id: 1, title: "독서", content: "IT책 10장 읽기", isDone: true },
-]);
+];
 
 const Todo = () => {
   const param = useParams();
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ToDoList from "../pages/ToDoList";
+import Todo from "../pages/Todo";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         {<Route path="/" element={<ToDoList />} />}
+        {<Route path="/:id" element={<Todo />} />}
         {/* <Route path="/" element={<ToDoList />} /> */}
         {/* <Route path="/:id" element={<Work />} /> */}
         {/* work 대신 Todo 넣기(import하고) */}
