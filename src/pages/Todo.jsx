3ca852e3bf17b0aec898411d78136f53c66c7231
YS739 <suk921@gmail.com>
2022-12-13 16:05:03 +0900

    trying to connect todo page - use param

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,3 +1,18 @@
 // todo 상세 페이지
 
-// export - router에서 import 하기
+import { useParams } from "react-router-dom";
+
+const [toDoS, setToDoS] = useState([
+  { id: 0, title: "리액트", content: "리액트 과제 다시 하기", isDone: false },
+  { id: 1, title: "독서", content: "IT책 10장 읽기", isDone: true },
+]);
+
+const Todo = () => {
+  const param = useParams();
+
+  const todo = toDoS.find((todo) => todo.id === parseInt(param.id));
+
+  return <div>{(todo.title, todo.content)}</div>;
+};
+
+export default Todo;
