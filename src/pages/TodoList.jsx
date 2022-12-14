495d10eebae08e1a305a8cfcf4024236ba365763
YS739 <suk921@gmail.com>
2022-12-14 19:16:46 +0900

    add Layout

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "../components/Header";
 import AddForm from "../components/AddForm";
 import TodoContainer from "../components/TodoContainer";
 import styled from "styled-components";
@@ -7,8 +6,6 @@ import styled from "styled-components";
 const ToDoList = () => {
   return (
     <div>
-      <Header />
-
       {/* ToDo 추가하는 input form */}
       <AddForm />
 
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,14 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ToDoList from "../pages/ToDoList";
 import Todo from "../pages/Todo";
+import Layout from "./Layout";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        {<Route path="/" element={<ToDoList />} />}
-        {<Route path="/:id" element={<Todo />} />}
-      </Routes>
+      <Layout>
+        <Routes>
+          {<Route path="/" element={<ToDoList />} />}
+          {<Route path="/:id" element={<Todo />} />}
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
