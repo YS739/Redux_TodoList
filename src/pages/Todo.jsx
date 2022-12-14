503b3c31c1ecc99b3c0fc7f85714c48f99a536cb
YS739 <suk921@gmail.com>
2022-12-14 15:42:57 +0900

    edit detail page

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -4,18 +4,15 @@ import { useSelector } from "react-redux";
 
 const Todo = () => {
   const global = useSelector((state) => state.TodoS.todo);
-  console.log(global);
   const param = useParams();
-  console.log(param);
 
   const theTodo = global.find((list) => list.id === param.id);
-  console.log(theTodo);
 
   return (
     <div>
       <div>
-        ID: {theTodo.id}
-        <Link to="/">Todo List</Link>
+        ID: {theTodo.id.slice(0, 8)}
+        <Link to="/"> Go Todo List</Link>
       </div>
       <div>{theTodo.title}</div>
       <div>{theTodo.content}</div>
